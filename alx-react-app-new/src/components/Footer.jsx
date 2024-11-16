4d38df364af0228f6b4680311b9a3438bf799ecf
Precious1298 <Okumoonoriode8@gmail.com>
2024-11-16 05:19:54 -0800

    Delete alx-react-app-new directory

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer () {
-    return (
-        <footer>
-            <p style={{backgroundColor: 'white', color: 'black', textAlign: 'center', fontSize: '20px'}}>© 2023 City Lovers</p>
-        </footer>
-    )
-}
--- alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,7 +0,0 @@
-export default function MainContent () {
-    return (
-        <main>
-            <p style={{backgroundColor: 'lightgreen', color: 'white', textAlign: 'center', fontSize: '20px'}}>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    )
-}
